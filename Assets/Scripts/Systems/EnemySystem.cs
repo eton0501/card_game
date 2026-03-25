3bdd7e47c2670cb663d4e67154744f5c124b2a36
eton0501 <etonn103@gmail.com>
2026-03-25 20:09:36 +0800

    增加敵人攻擊動畫

--- Assets/Scripts/Systems/EnemySystem.cs
+++ Assets/Scripts/Systems/EnemySystem.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using UnityEngine;
 using System.Collections.Generic;
+using DG.Tweening;
+using Unity.Collections;
 
 public class EnemySystem : Singleton<EnemySystem>
 {
     [SerializeField] private EnemyBoardView enemyBoardView;
     void OnEnable()
     {
-       ActionSystem.AttachPerformer<EnemyTurnGA>(EnemyTurnPerformer); 
+       ActionSystem.AttachPerformer<EnemyTurnGA>(EnemyTurnPerformer);
+       ActionSystem.AttachPerformer<AttackHeroGA>(AttackHeroPerformer); 
     }
     void OnDisable()
     {
         ActionSystem.DetachPerformer<EnemyTurnGA>();
+        ActionSystem.DetachPerformer<AttackHeroGA>();
     }
     public void Setup(List<EnemyData> enemyDatas)
     {
@@ -22,8 +26,20 @@ public class EnemySystem : Singleton<EnemySystem>
     }
     private IEnumerator EnemyTurnPerformer(EnemyTurnGA enemyTurnGA)
     {
-        Debug.Log("Enemy Turn");
-        yield return new WaitForSeconds(2f);
-        Debug.Log("end Enemy Turn");
+        foreach(var enemy in enemyBoardView.EnemyViews)
+        {
+           AttackHeroGA attackHeroGA=new(enemy);
+           ActionSystem.Instance.AddReaction(attackHeroGA);
+        }
+        yield return null;
+    }
+    private IEnumerator AttackHeroPerformer(AttackHeroGA attackHeroGA)
+    {
+        EnemyView attacker=attackHeroGA.Attacker;
+        Tween tween=attacker.transform.DOMoveX(attacker.transform.position.x-1f,0.15f);
+        yield return tween.WaitForCompletion();
+        attacker.transform.DOMoveX(attacker.transform.position.x+1f,0.25f);
+        DealDamageGA dealDamageGA=new(attacker.AttackPower,new(){HeroSystem.Instance.HeroView});
+        ActionSystem.Instance.AddReaction(dealDamageGA);
     }
 }
